1b28fc0cb570a0aebe2ea08ce39b79b10c421e4e
c0a21025 <c0a21025ab@edu.teu.ac.jp>
2022-12-06 14:26:09 +0900

    rensyu3

--- ex03/maze.py
+++ ex03/maze.py
@@ -4,5 +4,9 @@ if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷えるこうかとん")
     canvas = tk.Canvas(root, width = 1500, height = 900, bg = "black")
+    image = tk.PhotoImage(file="fig/3.png")
+    cx, cy = 300, 400
+    canvas.create_image(cx,cy,image=image, 
+                        tag="koukaton")
     canvas.pack()
     root.mainloop()
