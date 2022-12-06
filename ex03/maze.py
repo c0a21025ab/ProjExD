ffda7451fe6d783f79075d0e5a42548b6df20ca4
c0a21025 <c0a21025ab@edu.teu.ac.jp>
2022-12-06 16:45:32 +0900

    ４限追加機能実装完了

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import maze_maker
+import tkinter.messagebox as tkm
 
 def key_down(event):
     global key
@@ -21,8 +22,31 @@ def main_proc():
         if key == "Left":mx += 1
         if key == "Right":mx -= 1
     cx , cy = mx * 100 +50 , my * 100 + 50
-    canvas.coords("koukaton",cx, cy)
+    print(cx,cy)
+    canvas.coords("ざんねんな鳥",cx, cy)
+    goal()
     root.after(100,main_proc)
+
+def goal():
+    global mx, my, maze_list, cx, cy
+    if mx == 13 and my == 7:
+        tkm.showinfo("goal","たどり着きました")
+        #canvas = tk.Canvas(root, width = 1500, height = 900, bg = "black")
+        #canvas.pack()
+        maze_list = maze_maker.make_maze(15,9)
+        mx, my = 1,1
+        cx , cy = mx * 100 +50 , my * 100 + 50
+        maze_maker.show_maze(canvas, maze_list)
+        #image = tk.PhotoImage(file="fig/3.png")
+        canvas.delete("ざんねんな鳥")
+        canvas.create_image(cx,cy,image=image, 
+                          tag="ざんねんな鳥")
+        canvas.coords("ざんねんな鳥",cx, cy)
+        key = ""
+        root.bind("<KeyPress>",key_down)
+        root.bind("<KeyRelease>",key_up)        
+        
+        
 
 if __name__ == "__main__":
     root = tk.Tk()
@@ -36,7 +60,7 @@ if __name__ == "__main__":
     maze_maker.show_maze(canvas, maze_list)
     image = tk.PhotoImage(file="fig/3.png")
     canvas.create_image(cx,cy,image=image, 
-                        tag="koukaton")
+                        tag="ざんねんな鳥")
     key = ""
     root.bind("<KeyPress>",key_down)
     root.bind("<KeyRelease>",key_up)
