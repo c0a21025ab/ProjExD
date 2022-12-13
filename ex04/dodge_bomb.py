24139d876dfcc4d4e08e7b31362b2a64b8e1acc0
c0a21025 <c0a21025ab@edu.teu.ac.jp>
2022-12-13 14:54:32 +0900

    rensyu4-5and6

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -1,19 +1,29 @@
 import pygame as pg
 import sys
+import random
 
 def main():
     clock = pg.time.Clock()
 
     pg.display.set_caption("逃げろ！こうかとん")
     scrn_sfc = pg.display.set_mode((1600,900))
+    scrn_rct = scrn_sfc.get_rect()
     pg_bg_sfc = pg.image.load("fig/pg_bg.jpg")
     pg_bg_rct = pg_bg_sfc.get_rect()
 
     tori_sfc = pg.image.load("fig/6.png")
     tori_sfc = pg.transform.rotozoom(tori_sfc, 0, 2.0)
     tori_rct = tori_sfc.get_rect()
-    tori_rct.center = 900,300
+    tori_rct.center = 900,400
     scrn_sfc.blit(tori_sfc, tori_rct)
+
+    bomb_sfc = pg.Surface((20,20))
+    bomb_sfc.set_colorkey((0,0,0))
+    pg.draw.circle(bomb_sfc, (255,0,0), (10,10),10)
+    bomb_rct = bomb_sfc.get_rect()
+    bomb_rct.centerx = random.randint(0,scrn_rct.width)
+    bomb_rct.centery = random.randint(0,scrn_rct.height)
+    scrn_sfc.blit(bomb_sfc, bomb_rct)
 
     while True:
         scrn_sfc.blit(pg_bg_sfc, pg_bg_rct)
@@ -32,6 +42,9 @@ def main():
             tori_rct.centerx += 1
         scrn_sfc.blit(tori_sfc, tori_rct)
         
+        vx,vy = +1,+1
+        bomb_rct.move_ip(vx,vy)
+        scrn_sfc.blit(bomb_sfc, bomb_rct)
         pg.display.update()
         clock.tick(1000)
 
